25ad4d67929bf81f0d5f456142d8bb7830ab3a4a
Aigen80 <aomurm@gmail.com>
2025-09-18 10:18:50 +0200

    Created a Car project

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@
             Console.WriteLine($"Hästkrafter {myCar.HorsePower}");
             Console.WriteLine($"Växlar {myCar.Gears}");
             Console.WriteLine($"Motorn är startad {myCar.EngineOn}");
-            Console.WriteLine();
+            Console.WriteLine(); // Cars separation
 
         }
     }
